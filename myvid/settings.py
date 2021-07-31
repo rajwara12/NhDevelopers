f617274d2f6697f77c960724b3544dc849d64379
saadan <msaadanwaqar@gmail.com>
2021-07-31 01:43:10 -0700

    staticfile1s

--- myvid/settings.py
+++ myvid/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS =   [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 MESSAGE_TAGS= {
     messages.ERROR:'danger'
 } 
